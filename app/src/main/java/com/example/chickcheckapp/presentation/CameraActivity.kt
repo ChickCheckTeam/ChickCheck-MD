47d367e5df2c8a3958277cf8bed554e3c63aa684
AdiSastrawan <adisastrawan110703@gmail.com>
2024-05-24 21:16:50 +0800

    feat:adding analysis page

--- app/src/main/java/com/example/chickcheckapp/presentation/CameraActivity.kt
+++ app/src/main/java/com/example/chickcheckapp/presentation/CameraActivity.kt
@@ -16,124 +16,26 @@ import androidx.camera.core.resolutionselector.AspectRatioStrategy
 import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import com.example.chickcheckapp.databinding.PreviewLayoutBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.chickcheckapp.R
+import com.example.chickcheckapp.databinding.ActivityCameraBinding
+import com.example.chickcheckapp.databinding.ActivityMainBinding
 import com.google.common.util.concurrent.ListenableFuture
 import java.io.File
 import java.util.ArrayList
 
 class CameraActivity : AppCompatActivity() {
-    private var _binding: PreviewLayoutBinding? = null
+    private var _binding: ActivityCameraBinding? = null
     private val binding get() = _binding!!
-    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
-    private var imageCapture: ImageCapture? = null
-    private var currentImage: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = PreviewLayoutBinding.inflate(layoutInflater)
+        _binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if (checkPermission(Manifest.permission.CAMERA)) {
-            startCamera()
-            binding.btnCapture.setOnClickListener {
-                if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) && checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                ) {
-                    captureImage()
-                }else{
-                    val listOfPermissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-                    takePictureLauncher.launch(listOfPermissions)
-
-                }
-            }
-        } else {
-            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-        }
-    }
-
-    private fun bindPreview(cameraProvider: ProcessCameraProvider?) {
-        val preview: androidx.camera.core.Preview = androidx.camera.core.Preview.Builder().build()
-        val cameraSelector: androidx.camera.core.CameraSelector =
-            androidx.camera.core.CameraSelector.Builder()
-                .requireLensFacing(androidx.camera.core.CameraSelector.LENS_FACING_BACK)
-                .build()
-        preview.setSurfaceProvider(binding.pvCamera.surfaceProvider)
-        val resolutionSelector = ResolutionSelector.Builder()
-            .setAspectRatioStrategy(AspectRatioStrategy.RATIO_16_9_FALLBACK_AUTO_STRATEGY)
-            .build()
-        val imageAnalyzer = ImageAnalysis.Builder()
-            .setResolutionSelector(resolutionSelector)
-            .setTargetRotation(binding.pvCamera.display.rotation)
-            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-            .build()
-        imageCapture = ImageCapture.Builder()
-            .setTargetRotation(binding.pvCamera.display.rotation)
-            .build()
-
-        cameraProvider?.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer, imageCapture)
-    }
-
-    private fun captureImage() {
-        val filename = "${System.currentTimeMillis()}"
-        val destination = File.createTempFile(
-            filename,".jpg",getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        )
-        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(destination).build()
-        imageCapture?.takePicture(
-            outputFileOptions,
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    currentImage = outputFileResults.savedUri
-                    binding.ivCapturedImage.setImageURI(currentImage)
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    showToast(exception.message.toString())
-                }
-
-            })
-    }
-
-    private fun startCamera() {
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture?.addListener({
-            val cameraProvider = cameraProviderFuture?.get()
-            bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(this))
 
     }
 
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            startCamera()
-        } else {
-            showToast("Permission required")
-        }
-    }
 
-    private val takePictureLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
-            val allGranted = result.all { it.value }
-
-            if (allGranted) {
-                captureImage()
-            } else {
-                showToast("Storage permission Required")
-            }
-        }
-
-
-    private fun checkPermission(permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            permission
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
 
 }
---
+++ app/src/main/java/com/example/chickcheckapp/presentation/ui/camera/CameraXFragment.kt
@@ -0,0 +1,151 @@
+package com.example.chickcheckapp.presentation.ui.camera
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.chickcheckapp.databinding.FragmentCameraxBinding
+import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.util.concurrent.Future
+
+class CameraXFragment : Fragment() {
+    private var _binding : FragmentCameraxBinding? = null
+    private val binding get() = _binding!!
+    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
+    private var imageCapture: ImageCapture? = null
+    private var currentImage: Uri? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCameraxBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (checkPermission(Manifest.permission.CAMERA)) {
+            startCamera()
+            binding.btnCapture.setOnClickListener {
+                if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) && checkPermission(
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                ) {
+                    captureImage()
+                }else{
+                    val listOfPermissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    takePictureLauncher.launch(listOfPermissions)
+
+                }
+            }
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+    private fun bindPreview(cameraProvider: ProcessCameraProvider?) {
+        val preview: androidx.camera.core.Preview = androidx.camera.core.Preview.Builder().build()
+        val cameraSelector: androidx.camera.core.CameraSelector =
+            androidx.camera.core.CameraSelector.Builder()
+                .requireLensFacing(androidx.camera.core.CameraSelector.LENS_FACING_BACK)
+                .build()
+        preview.setSurfaceProvider(binding.pvCamera.surfaceProvider)
+        val resolutionSelector = ResolutionSelector.Builder()
+            .setAspectRatioStrategy(AspectRatioStrategy.RATIO_16_9_FALLBACK_AUTO_STRATEGY)
+            .build()
+        val imageAnalyzer = ImageAnalysis.Builder()
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(binding.pvCamera.display.rotation)
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+            .build()
+        imageCapture = ImageCapture.Builder()
+            .setTargetRotation(binding.pvCamera.display.rotation)
+            .build()
+
+        cameraProvider?.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer, imageCapture)
+    }
+
+    private fun captureImage() {
+        val filename = "${System.currentTimeMillis()}"
+        val destination = File.createTempFile(
+            filename,".jpg",requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        )
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(destination).build()
+        imageCapture?.takePicture(
+            outputFileOptions,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    currentImage = outputFileResults.savedUri
+                    val action = CameraXFragmentDirections.actionCameraXFragmentToAnalysisFragment(currentImage.toString())
+                    view?.findNavController()?.navigate(action)
+                    binding.ivCapturedImage.setImageURI(currentImage)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    showToast(exception.message.toString())
+                }
+
+            })
+    }
+
+    private fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture?.addListener({
+            val cameraProvider = cameraProviderFuture?.get()
+            cameraProvider?.unbindAll()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            startCamera()
+        } else {
+            showToast("Permission required")
+        }
+    }
+
+    private val takePictureLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+            val allGranted = result.all { it.value }
+
+            if (allGranted) {
+                captureImage()
+            } else {
+                showToast("Storage permission Required")
+            }
+        }
+
+
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+}
