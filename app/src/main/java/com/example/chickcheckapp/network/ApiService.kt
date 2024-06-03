4eded6fe4e33a8293728b0d57045eb2a98d9f26a
harrysudanaa <komangharry02@gmail.com>
2024-06-04 00:33:53 +0800

    add logout from api

--- app/src/main/java/com/example/chickcheckapp/network/ApiService.kt
+++ app/src/main/java/com/example/chickcheckapp/network/ApiService.kt
@@ -4,11 +4,13 @@ import android.view.PixelCopy.Request
 import com.example.chickcheckapp.data.remote.request.LoginRequest
 import com.example.chickcheckapp.data.remote.request.SignUpRequest
 import com.example.chickcheckapp.data.remote.response.LoginResponse
+import com.example.chickcheckapp.data.remote.response.LogoutResponse
 import com.example.chickcheckapp.data.remote.response.SignupResponse
 import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -23,5 +25,10 @@ interface ApiService {
     suspend fun login(
         @Body requestBody: LoginRequest
     ): LoginResponse
+
+    @POST("logout")
+    suspend fun logout(
+        @Header("Authorization") token: String
+    ): LogoutResponse
 
 }
